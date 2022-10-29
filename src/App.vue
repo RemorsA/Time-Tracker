6142314c01da7651de6b61956a3f19534d2e55f7
IlyaRemorsA <blackbaggetjack@gmail.com>
2022-10-29 19:17:39 +0300

    new version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-container fluid style="width: 700px">
+
+            <NavComponent
+                @dialog="dialog = true"
+                @clearCards="clearCards"
+                @searchCard="searchCard"
+            />
+            <TimeLineComponent
+                v-if="cards.length !== 0"
+                :cards="cards"
+            />
+
+            <v-alert
+                v-else
+                type="info"
+                border="bottom"
+                dark
+                colored-border
+                color="yellow"
+            >Записей нет !</v-alert>
+
+            <v-dialog
+                v-model="dialog"
+                width="500"
+            >
+
+                <v-card dark>
+                    <v-card-title>
+                        <v-text-field
+                            hint="Название"
+                            outlined
+                            clearable
+                        />
+                    </v-card-title>
+
+                    <v-col>
+                        <v-textarea
+                            outlined
+                            hint="Описание задачи"
+                            rows="20"
+                            clearable
+                            no-resize
+                        />
+                    </v-col>
+
+                    <v-card-actions>
+                        <v-col cols="2">
+                            <v-btn dark>
+                                <v-icon color="blue">mdi-content-save</v-icon>
+                                <!-- <v-icon size="30px">mdi-content-save-edit</v-icon> -->
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="2">
+                            <v-btn dark>
+                                <v-icon color="yellow">mdi-reload</v-icon>
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="2">
+                            <v-btn dark>
+                                <v-icon color="green">mdi-play</v-icon>
+                                <!-- <v-icon size="30px">mdi-pause</v-icon> -->
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="4">
+                            <p style="font-size: 20px; color: white;">1.ч 10.м 40.с</p>
+                        </v-col>
+                        <v-col cols="1">
+                            <v-btn color="red" @click="dialog = false">
+                                <v-icon size="30px">mdi-close</v-icon>
+                            </v-btn>
+                        </v-col>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+        </v-container>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimeLineComponent from '@/components/TimeLineComponent.vue'
+import NavComponent from '@/components/NavComponent.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        TimeLineComponent,
+        NavComponent,
+    },
+
+    data() {
+        return {
+            dialog: false,
+            cards: []
+        }
+    },
+
+    watch: {
+        dialog() {
+            if (!this.dialog) {
+                console.log('CLOSE DIALOG')
+            }
+        }
+    },
+
+    methods: {
+        addCard() {
+            
+        },
+        clearCards() {
+            this.cards = []
+        },
+        searchCard(value) {
+            console.log(value)
+        }
+    },
+
+    created() {
+        
+    }
 }
 </script>
 
-<style lang="scss">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background: #121212;
+    font-family: 'Montserrat', sans-serif;
 }
 </style>
---
+++ src/components/TimeLineComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-row>
+        <v-col cols="20">
+
+            <v-timeline dark dense>
+
+                <v-slide-x-reverse-transition group hide-on-leave>
+
+                    <v-timeline-item
+                        color="pink"
+                        v-for="(i, index) in cards"
+                        :key="index"
+                    >
+
+                        <v-col style="
+                            display: flex;
+                            align-items: center;
+                        ">
+                            <v-col cols="3">
+                                <p style="color: white">{{i.date}}</p>
+                                <p style="color: white">{{i.time}}</p>
+                            </v-col>
+
+                            <v-card dark>
+                                <v-card-title>
+                                    {{i.title}}
+                                </v-card-title>
+                                <v-col>
+                                    <p>{{i.desription}}</p>
+                                </v-col>
+                            </v-card>
+                        </v-col>
+                    </v-timeline-item>
+
+                </v-slide-x-reverse-transition>
+
+            </v-timeline>
+
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+
+export default {
+    props: ['cards'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
